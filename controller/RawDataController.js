83e2731f6922fe005278319dd5d95f048ee2aa98
Puritat Chamart <108642001+introbond@users.noreply.github.com>
2022-10-31 14:07:41 +0000

    error handling case

--- controller/RawDataController.js
+++ controller/RawDataController.js
@@ -1,45 +1,60 @@
 const service = require('../util/service');
 
 exports.getAllData = (async (req, res) => {
-    const rawData = await service.getRawData();
-    res.json({
-        success: true,
-        data: rawData,
-    });
+    try {
+        const rawData = await service.getRawData();
+        res.json({
+            success: true,
+            data: rawData,
+        });
+    } catch {
+        res.status(500).send('internal server error');
+    }
 });
 
 exports.getMinMaxAverage = (async (req, res) => {
-    const rawData = await service.getRawData();
-    const maxData = Math.max.apply(Math, rawData.map(a => a.data));
-    const minData = Math.min.apply(Math, rawData.map(a => a.data));
-    const averageData = rawData.reduce((total, current) => total + current.data, 0) / rawData.length;
-    res.json({
-        success: true,
-        min: minData,
-        max: maxData,
-        average: averageData.toFixed(2),
-    });
+    try {
+        const rawData = await service.getRawData();
+        const maxData = Math.max.apply(Math, rawData.map(a => a.data));
+        const minData = Math.min.apply(Math, rawData.map(a => a.data));
+        const averageData = rawData.reduce((total, current) => total + current.data, 0) / rawData.length;
+        res.json({
+            success: true,
+            min: minData,
+            max: maxData,
+            average: averageData.toFixed(2),
+        });
+    } catch {
+        res.status(500).send('internal server error');
+    }
 });
 
 exports.getDataByRange = (async (req, res) => {
-    const rangeIndex = Number(req.query.rangeIndex);
-    const rawData = await service.getRawData();
-    const maxData = Math.max.apply(Math, rawData.map(a => a.data));
-    const minData = Math.min.apply(Math, rawData.map(a => a.data));
-    const fullRange = maxData - minData;
-    const subRangeNumber = Math.ceil(fullRange / 200);
-    const subRangeValue = []
-    let buffer = minData;
-    for (let i = 0; i < subRangeNumber; i++) {
-        buffer = buffer + 200;
-        subRangeValue.push(buffer);
-    };
-    const filter = rawData.filter((item) => {
-        return (item.data >= subRangeValue[rangeIndex]) && (item.data <= subRangeValue[rangeIndex+1]);
-    });
-
-    res.json({
-        success: true,
-        filter: filter
-    });
+    try {
+        const rangeIndex = Number(req.query.rangeIndex);
+        const rawData = await service.getRawData();
+        const maxData = Math.max.apply(Math, rawData.map(a => a.data));
+        const minData = Math.min.apply(Math, rawData.map(a => a.data));
+        const fullRange = maxData - minData;
+        const subRangeNumber = Math.ceil(fullRange / 200);
+        const subRangeValue = []
+        let buffer = minData;
+        for (let i = 0; i < subRangeNumber; i++) {
+            buffer = buffer + 200;
+            subRangeValue.push(buffer);
+        };
+        if (rangeIndex >= (subRangeValue.length - 1)) {
+            res.status(400).send('no data in range')
+        }
+        const filter = rawData.filter((item) => {
+            return (item.data >= subRangeValue[rangeIndex]) && (item.data <= subRangeValue[rangeIndex+1]);
+        });
+    
+        res.json({
+            success: true,
+            filter: filter
+        });
+    } catch {
+        res.status(500).send('internal server error');
+    }
 });
