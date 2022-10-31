ebfe7d065d15c6801d8632e878c592aca0799756
introbond <introbond.dev@gmail.com>
2022-10-31 19:46:44 +0700

    add ejs

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(morgan('tiny'));
 
 const data = require('./route/RawData');
-
 app.use('/api/v1', data);
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from server');
+    res.status(200).render('index');
 });
 
 module.exports = app;
